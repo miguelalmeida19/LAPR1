abc70bd43f01bedd74b880f5abb79704ff65a788
Miguel Almeida <72946916+miguelalmeida19@users.noreply.github.com>
2022-08-07 16:35:16 +0100

    Projeto

---
+++ 1201114-lapr1_turmadij_grupo07-27ad3d74601f/src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    //método
+    /*
+    no início de cada método, colocar sob a forma de comentário curtinho, a explicação do método.
+    */
+
+    //método
+
+    //método
+}
